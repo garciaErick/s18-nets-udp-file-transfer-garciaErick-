9763624cc2a31aa7492b8f2df3e284b6bdc9f846
Erick Garcia <garcia.erick.cs@gmail.com>
2018-02-12 02:11:34 -0700

    Refractored client and server and included meaningful messages

--- proxy/udpClient.py
+++ proxy/udpClient.py
@@ -1,42 +1,49 @@
 #! /bin/python
 from socket import *
+import sys
+import re
 
 # default params
 serverAddr = ('localhost', 50000)
 
-import sys, re
 
 def usage():
-    print "usage: %s [--serverAddr host:port]"  % sys.argv[0]
+    print "usage: %s [--serverAddr host:port]" % sys.argv[0]
     sys.exit(1)
 
-def getMethod(textFname):
+
+def get_method(textFname):
     print("Creating file: ")
-    with open(textFname, 'w') as inputFile: #Add to the dictionary the words found in the file.
+    with open(textFname, 'w') as inputFile:  # Add to the dictionary the words found in the file.
         inputFile.write('i play to win')
 
-def putMethod(textFname):
-    print("Uploading file")
+
+def put_method(textFname):
+    print("Initializing PUT from client")
     clientSocket = socket(AF_INET, SOCK_DGRAM)
     with open(textFname, 'r') as inputFile:
         for line in inputFile:
-            line= line.strip()
+            line = line.strip()
             message = line
             clientSocket.sendto(message, serverAddr)
-            modifiedMessage, serverAddrPort = clientSocket.recvfrom(2048)
-            print "Modified message from %s is <%s>" % (repr(serverAddrPort), modifiedMessage)
+        modifiedMessage, serverAddrPort = clientSocket.recvfrom(2048)
+        print "Message from %s is: %s" % (repr(serverAddrPort), modifiedMessage)
+        # print "Modified message from %s is <%s>" % (repr(serverAddrPort), modifiedMessage)
+
 
 try:
     args = sys.argv[1:]
     while args:
-        sw = args[0]; del args[0]
+        sw = args[0]
+        del args[0]
         if sw == "--serverAddr":
-            addr, port = re.split(":", args[0]); del args[0]
+            addr, port = re.split(":", args[0])
+            del args[0]
             serverAddr = (addr, int(port))
         else:
             print "unexpected parameter %s" % args[0]
-            usage();
+            usage()
 except:
     usage()
 
-putMethod("testing.txt")
+put_method("testing.txt")
--- proxy/udpServer.py
+++ proxy/udpServer.py
@@ -5,19 +5,24 @@ from socket import *
 serverAddr = ("", 50001)
 
 import sys
+
+
 def usage():
-    print "usage: %s [--serverPort <port>]"  % sys.argv[0]
+    print "usage: %s [--serverPort <port>]" % sys.argv[0]
     sys.exit(1)
+
 
 try:
     args = sys.argv[1:]
     while args:
-        sw = args[0]; del args[0]
+        sw = args[0]
+        del args[0]
         if sw == "--serverPort":
-            serverAddr = ("", int(args[0])); del args[0]
+            serverAddr = ("", int(args[0]))
+            del args[0]
         else:
             print "unexpected parameter %s" % args[0]
-            usage();
+            usage()
 except:
     usage()
 
@@ -28,10 +33,13 @@ serverSocket.bind(serverAddr)
 print "ready to receive"
 with open("plswork.txt", 'w') as outputFile:
     while 1:
-        message, clientAddrPort = serverSocket.recvfrom(2048)
-        modifiedMessage = message
-        outputFile.write(message + "\n")
-        print "from %s: rec'd '%s'" % (repr(clientAddrPort), message)
-        serverSocket.sendto(modifiedMessage, clientAddrPort)
-        outputFile.flush()
-    outputFile.close()
+        try:
+            message, clientAddrPort = serverSocket.recvfrom(2048)
+            modifiedMessage = message
+            outputFile.write(message + "\n")
+            outputFile.flush()
+            # print "from %s: rec'd '%s'" % (repr(clientAddrPort), message)
+            # serverSocket.sendto(modifiedMessage, clientAddrPort)
+        finally:
+            modifiedMessage = "Successfully made put request"
+            serverSocket.sendto(modifiedMessage, clientAddrPort)
